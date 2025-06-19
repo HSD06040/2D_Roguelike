1dc2de850dd274c7287eef069f1033c421e097fd
JangHyeonTae <141475959+JangHyeonTae@users.noreply.github.com>
2025-06-19 16:04:56 +0900

    [Feat] Test/JHT
    test/Jang

---
+++ Assets/Student/JHT/Scripts/JHT_test.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JHT_test : MonoBehaviour
+{
+    public int testint = 0;
+
+    void Start()
+    {
+        Debug.Log(testint);
+    }
+
+}
