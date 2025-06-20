568427e1713a61cbdf6aa4d814c2535b95c34c86
HSD06040 <tinto0604@gmail.com>
2025-06-20 16:37:45 +0900

    [Feat] 데이터 다운로더 제작 및 테스트완료
    구글 시트의 URL을 통해 가져오도록 하였다

--- Assets/Scripts/Manager/DataManager.cs
+++ Assets/Scripts/Manager/DataManager.cs
@@ -2,7 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DataManager : Singleton<DataManager>
 {
-    
+    public Weapon[] WeaponDatas;
+    private DataDownloader downloader;
+
+    private void Awake()
+    {
+        WeaponDatas = Resources.LoadAll<Weapon>("Data/Weapon");
+
+        downloader = new DataDownloader();
+        StartCoroutine(downloader.DownloadData());     
+    }
 }
