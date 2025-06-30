8a2532bdf5b3cba1b979cfdc748d23940dbe45d8
woojinjang193 <ryanjang193@gmail.com>
2025-07-01 02:23:40 +0900

    [feat] 다이얼로그매니저 수정, 오프닝 다이얼로그 추가

--- Assets/Student/JWJ/Scripts/DialogueManager.cs
+++ Assets/Student/JWJ/Scripts/DialogueManager.cs
@@ -17,13 +17,17 @@ public class DialogueData
 {
     public string dialogue; //대사 내용
     public DialoguePosition position; // 출력할 위치
-    public string speaker;
+    public string speaker; //말하는 인물 이름
+    public DialogueEffect effect; // Fade In 이나 Typing
+    public DialogueAdvanceType advanceType; // 인풋으로 넘길지 자동으로 넘길지
 
-    public DialogueData(string speaker, string line, DialoguePosition position) //생성자, 다이얼로그 출력중 변경될 데이터들
+    public DialogueData(string speaker, string line, DialoguePosition position, DialogueEffect effect, DialogueAdvanceType advanceType) //생성자, 다이얼로그 출력중 변경될 데이터들
     {
         this.dialogue = line;
         this.position = position;
         this.speaker = speaker;
+        this.effect = effect;
+        this.advanceType = advanceType;
     }
 }
 
@@ -43,7 +47,8 @@ public class DialogueManager : MonoBehaviour//Singleton<DialogueManager>
     [Header("효과 설정")]
     [SerializeField] private float fadeInDuration = 0.5f;
     [SerializeField] private float typingSpeed = 0.05f;
-    [SerializeField] private float autoAdvanceDelay = 2f;
+    [SerializeField] private float fadeOutDuration = 2f;
+    [SerializeField] private float typingRemovalTime = 2f; 
 
     [Header("스킵버튼")]
     [SerializeField] private GameObject skipButton;
@@ -83,7 +88,7 @@ public class DialogueManager : MonoBehaviour//Singleton<DialogueManager>
     /// <param name="effect">문자 출력 효과(Fade In, Typing)</param>
     /// <param name="advanceType">대사 넘김 방식(Auto, Manual)</param>
     /// <param name="canSkip">전체 스킵 가능 여부 (true, false)</param>
-    public void ShowDialogue(DialogueData[] lines, DialogueEffect effect, DialogueAdvanceType advanceType, bool canSkip)
+    public void ShowDialogue(DialogueData[] lines, bool canSkip)
     {
         sentences.Clear(); //큐 비움
 
@@ -92,8 +97,6 @@ public class DialogueManager : MonoBehaviour//Singleton<DialogueManager>
             sentences.Enqueue(line);
         }
 
-        curEffect = effect; //현재 이펙트
-        curAdvanceType = advanceType; //진행 타입
         isSkipable = canSkip; //스킵가능여부
         skipButton.SetActive(isSkipable);  //스킵 가능이면 스킵버튼 활성화
 
@@ -104,7 +107,7 @@ public class DialogueManager : MonoBehaviour//Singleton<DialogueManager>
     {
         if(sentences.Count <= 0)  //출력할게 더이상 없으면
         {
-            //DialogueOver();
+            dialogueOver?.Invoke();
             return;
         }
 
@@ -121,7 +124,9 @@ public class DialogueManager : MonoBehaviour//Singleton<DialogueManager>
         }
 
         DialogueData line = sentences.Dequeue();
-        //nameText.text = line.speaker;
+        
+        curEffect = line.effect;
+        curAdvanceType = line.advanceType;
 
         if (line.position == DialoguePosition.Center)
         {
@@ -134,27 +139,25 @@ public class DialogueManager : MonoBehaviour//Singleton<DialogueManager>
             centerPanel.SetActive(false);
             bottomPanel.SetActive(true);
             currentText = bottomDialogueText;
-
-            
         }
 
         if(curEffect == DialogueEffect.FadeIn)  //이팩트가 페이드인이라면
         {
-            effectCoroutine = StartCoroutine(TextFadeIn(line.dialogue, line.speaker));
+            effectCoroutine = StartCoroutine(TextFadeIn(line.dialogue, line.speaker, line.advanceType));
         }
 
         else if(curEffect == DialogueEffect.Typing) //이팩트가 타이핑이라면
         {
-            effectCoroutine = StartCoroutine(TextTyping(line.dialogue));
+            effectCoroutine = StartCoroutine(TextTyping(line.dialogue, line.speaker, line.advanceType));
         }
     }
 
-    private IEnumerator TextFadeIn(string line, string speaker)  //페이드인 효과 코루틴
+    private IEnumerator TextFadeIn(string line, string speaker, DialogueAdvanceType advanceType)  //페이드인 효과 코루틴
     {
         nameText.text = speaker;
-        Color nameColor = nameText.color;
-        nameColor.a = 0;
-        nameText.color = nameColor;
+        //Color nameColor = nameText.color;
+        //nameColor.a = 0;
+        //nameText.color = nameColor;
 
         currentText.text = line;
         Color color = currentText.color;  // 현재텍스트의 컬러를 담아줌
@@ -166,33 +169,61 @@ public class DialogueManager : MonoBehaviour//Singleton<DialogueManager>
         {
             timer += Time.deltaTime;
             float alpha = Mathf.Lerp(0f, 1f, timer / fadeInDuration); //알파라는 변수를 선언하고 0에서 1까지 올려줌
-            nameText.color = new Color(nameColor.r, nameColor.g, nameColor.b, alpha);
+            //nameText.color = new Color(nameColor.r, nameColor.g, nameColor.b, alpha);
             currentText.color = new Color(color.r, color.g, color.b, alpha); //변수 알파를 색상 알파에 넣음
             yield return null;
         }
         color.a = 1f;  // 와일문이 끝나면 알파를 1로 확실히 지정해줌
-        nameColor.a = 1f;
+        //nameColor.a = 1f;
 
-        nameText.color = nameColor;
+        //nameText.color = nameColor;
         currentText.color = color;  //적용
 
-        SelectAdvanceType();
-    }
-
-    private IEnumerator TextTyping(string line) //타이핑 효과
-    {
-        currentText.text = ""; //한글자씩 나와야 하니까 디큐한 라인을 초기화해줌
-
-        foreach(char letter in line)
+        if (advanceType == DialogueAdvanceType.Auto)
         {
-            currentText.text += letter;
-            yield return new WaitForSeconds(typingSpeed);
+            StartCoroutine(AutoNextSentence());
+        }
+        else
+        {
+            isWaitingForInput = true;
         }
 
-        SelectAdvanceType();
     }
 
-    private void SelectAdvanceType()
+    private IEnumerator TextTyping(string line, string speaker, DialogueAdvanceType advanceType) //타이핑 효과
+    {
+        nameText.text = speaker;
+
+        currentText.text = ""; //한글자씩 나와야 하니까 디큐한 라인을 초기화해줌
+
+        string displayText = "";
+        string curColor = "white"; //기본색상
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (line[i] == 'r') { curColor = "red"; continue; }
+            if (line[i] == 'y') { curColor = "yellow"; continue; }
+            if (line[i] == 'b') { curColor = "blue"; continue; }
+            if (line[i] == 'g') { curColor = "green"; continue; }
+            if (line[i] == 'w') { curColor = "white"; continue; }
+        
+            displayText += $"<color={curColor}>{line[i]}</color>";
+            currentText.text = displayText;
+        
+            yield return new WaitForSeconds(typingSpeed);
+        
+        }
+
+       //foreach(char letter in line)
+       //{
+       //    currentText.text += letter;
+       //    yield return new WaitForSeconds(typingSpeed);
+       //}
+
+        SelectAdvanceType(advanceType);
+    }
+
+    private void SelectAdvanceType(DialogueAdvanceType advanceType)
     {
         if (curAdvanceType == DialogueAdvanceType.Auto)  //대사 진행 타입이 오토라면
         {
@@ -204,38 +235,62 @@ public class DialogueManager : MonoBehaviour//Singleton<DialogueManager>
         }
     }
 
-    private IEnumerator AutoNextSentence()  //자동으로 다음텍스트로 넘어감 (페이드아웃)
+    private IEnumerator AutoNextSentence()  //자동으로 다음텍스트로 넘어감
     {
-        Color nameColor = nameText.color;
-
-        Color color = currentText.color;
-        float timer = 0f;
-        while (timer < autoAdvanceDelay)  //페이드아웃으로 사라짐
+        if (curEffect == DialogueEffect.FadeIn)
         {
-            timer += Time.deltaTime;
-            float alpha = Mathf.Lerp(1f, 0f, timer / autoAdvanceDelay); 
-            currentText.color = new Color(color.r, color.g, color.b, alpha); 
-            nameText.color = new Color(nameColor.r, nameColor.g, nameColor.b, alpha);
-            yield return null;
-        }
+            //Color nameColor = nameText.color;
+            Color color = currentText.color;
+            float timer = 0f;
+            while (timer < fadeOutDuration)  //페이드아웃으로 사라짐
+            {
+                timer += Time.deltaTime;
+                float alpha = Mathf.Lerp(1f, 0f, timer / fadeOutDuration);
+                currentText.color = new Color(color.r, color.g, color.b, alpha);
+                //nameText.color = new Color(nameColor.r, nameColor.g, nameColor.b, alpha);
+                yield return null;
+            }
 
-        if (sentences.Count <= 0)
-        {
-            DialogueOver();
-            dialogueOver?.Invoke();
+            if (sentences.Count <= 0)
+            {
+                DialogueOver();
+                
+            }
+            else
+            {
+                DisplaySentence();
+            }
         }
         else
         {
+            yield return new WaitForSeconds(typingRemovalTime);
             DisplaySentence();
-        }  
+        }
     }
 
     private void InputToNextSentence() // 인풋이 있어야 다음 텍스트로 넘어감
     {
         if(Input.anyKeyDown)
         {
-            DisplaySentence();
             isWaitingForInput = false;
+
+            if (curEffect == DialogueEffect.FadeIn)
+            {
+                StartCoroutine(AutoNextSentence());
+            }
+            else
+            {
+                if(sentences.Count <= 0)
+                {
+                    DialogueOver();
+                    
+                }
+                else
+                {
+                    DisplaySentence();
+                }
+                
+            }
         }
     }
 
@@ -243,8 +298,6 @@ public class DialogueManager : MonoBehaviour//Singleton<DialogueManager>
     {
         StopAllCoroutines();
         DialogueOver();
-
-        dialogueOver?.Invoke();
     }
 
     private void DialogueOver() //다이얼로그 끝날때
@@ -254,5 +307,7 @@ public class DialogueManager : MonoBehaviour//Singleton<DialogueManager>
         bottomPanel.SetActive(false);
         skipButton.SetActive(false);
         isWaitingForInput = false;
+
+        dialogueOver?.Invoke();
     }
 }
--- Assets/Student/JWJ/Scripts/StartDialogue.cs
+++ Assets/Student/JWJ/Scripts/StartDialogue.cs
@@ -1,41 +1,73 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.Rendering.DebugUI;
 
 public class StartDialogue : MonoBehaviour
 {
     [SerializeField] private DialogueManager dialogueManager;
 
+    private bool hasPlayedStory2 = false;
 
+    private void OnEnable()
+    {
+        dialogueManager.dialogueOver += DialogueOver;
+    }
+
+    private void OnDisable()
+    {
+        dialogueManager.dialogueOver -= DialogueOver;
+    }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKeyDown(KeyCode.O))
         {
-            Prolog();
+            StartStory1();
         }
     }
-    private void Prolog()
+    private void StartStory1()
     {
-        Debug.Log("프롤로그 실행");
         DialogueData[] lines = new DialogueData[]
         {
-            new DialogueData("", "18XX년 XX월 XX일.", DialoguePosition.Center),
-            new DialogueData("<color=yellow>???</color> ", "왜 그 음악을 들어서..... 이런 일이 벌어졌을까...", DialoguePosition.Bottom),
-            new DialogueData("", "음악이 이 세상의 전부인 시기.", DialoguePosition.Center),
-            new DialogueData("<color=green>???</color>", "모든 게 다 끝났어...", DialoguePosition.Bottom),
-            new DialogueData("", "고통받는 시민들의 울부짖음이 불협을 이루며 거리마다 울리던 시기.", DialoguePosition.Center),
-            new DialogueData("<color=red>???</color>", "<color=red>아아.... 나를 구원할...신이시여...</color>", DialoguePosition.Bottom),
-            new DialogueData("", "신도들의 찢어질 듯한 환호성이 소극장을 가득 메우던 시기.", DialoguePosition.Center),
-            new DialogueData("<color=red>???</color>", "<color=red>이 선율로.... 우리는 새롭게 태어날 것입니다...</color>", DialoguePosition.Bottom),
-            new DialogueData("", "소환된 악마가 세상을 지배하던 시기.", DialoguePosition.Center),
-            new DialogueData("<color=red>???</color>", "<color=red>흑음악만이.... 토베벤님만이...\n 나를... 우리를... 세상을 정화할 것이니...</color>", DialoguePosition.Bottom),
-            new DialogueData("", "<color=red>흑음악</color>은 이 세상을 순식간에 지배하기 시작했다.", DialoguePosition.Center),
-            //페이드아웃효과
+            new DialogueData("<color=blue>나</color>", "정말 끔찍하군", DialoguePosition.Bottom, DialogueEffect.Typing, DialogueAdvanceType.Manual),
+            new DialogueData("<color=blue>나</color>", "지금까지 많은 마을을 지나오면서 이렇게까지 망가진 곳은 처음 보는데..", DialoguePosition.Bottom, DialogueEffect.Typing, DialogueAdvanceType.Manual),
+            new DialogueData("<color=yellow>???</color>", "백음악사님.. 도와주..세요.....", DialoguePosition.Bottom, DialogueEffect.Typing, DialogueAdvanceType.Manual),
+            new DialogueData("<color=blue>나</color>", "상처가 깊지 않아 다행이네요. 우선 이렇게만이라도…", DialoguePosition.Bottom, DialogueEffect.Typing, DialogueAdvanceType.Manual),
         };
-
-        dialogueManager.ShowDialogue(lines, DialogueEffect.FadeIn, DialogueAdvanceType.Auto, true);
+        dialogueManager.ShowDialogue(lines, false);
     }
 
-   
+    private void StartStory2()
+    {
+        DialogueData[] lines = new DialogueData[]
+        {
+            new DialogueData("<color=yellow>???</color>", "정말 감사합니다...", DialoguePosition.Bottom, DialogueEffect.Typing, DialogueAdvanceType.Manual),
+            new DialogueData("<color=blue>나</color>", "상황이 제일 심각한 곳이 어디죠?", DialoguePosition.Bottom, DialogueEffect.Typing, DialogueAdvanceType.Manual),
+            new DialogueData("<color=yellow>???</color>", "광장이라고 들었습니다.. 많이 위험할 거예요..", DialoguePosition.Bottom, DialogueEffect.Typing, DialogueAdvanceType.Manual),
+            new DialogueData("<color=blue>나</color>", "걱정 마세요.", DialoguePosition.Bottom, DialogueEffect.Typing, DialogueAdvanceType.Manual),
+        };
+        dialogueManager.ShowDialogue(lines, false);
+    }
+
+    private IEnumerator WaitForNextStory()
+    {
+        Debug.Log("화면 효과");
+        yield return new WaitForSeconds(2);
+        StartStory2();
+    }
+
+    private void DialogueOver()
+    {
+        if(!hasPlayedStory2)
+        {
+            hasPlayedStory2 = true;
+            StartCoroutine(WaitForNextStory());
+        }
+        else
+        {
+
+        }    
+        
+    }
 }
