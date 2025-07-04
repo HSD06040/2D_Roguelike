2df5237113cd24b2e3a5d0895348eb389d67d27b
JangHyeonTae <141475959+JangHyeonTae@users.noreply.github.com>
2025-07-04 16:46:08 +0900

    [feat] Input system for Show Status UI
    인풋시스템을 넣었습니다 (Player Status UI)

--- Assets/Scripts/UI/Weapon/WeaponSlotPresenter.cs
+++ Assets/Scripts/UI/Weapon/WeaponSlotPresenter.cs
@@ -5,10 +5,16 @@ using UnityEngine;
 public class WeaponSlotPresenter
 {
     private WeaponSlot weaponSlot;
+    private StatusPopUp statusPopUp;
 
     public WeaponSlotPresenter(WeaponSlot _weaponSlot)
     {
         weaponSlot = _weaponSlot;
+    }
+
+    public WeaponSlotPresenter(StatusPopUp _statusPopUp)
+    {
+        statusPopUp = _statusPopUp;
     }
 
     public void AddEvent()
@@ -22,4 +28,18 @@ public class WeaponSlotPresenter
         Manager.Data.PlayerStatus.OnChangedWeapon -= weaponSlot.UpdateWeaponSlot;
         Manager.Data.PlayerStatus.OnCurrentWeaponChanged -= weaponSlot.ChangeSlot;
     }
+
+    #region StatusPopUp
+
+    public void AddStatusEvent()
+    {
+        Manager.Data.PlayerStatus.OnChangedWeapon += statusPopUp.UpdateWeaponData;
+    }
+
+    public void RemoveStatusEvent()
+    {
+        Manager.Data.PlayerStatus.OnChangedWeapon -= statusPopUp.UpdateWeaponData;
+    }
+
+    #endregion
 }
