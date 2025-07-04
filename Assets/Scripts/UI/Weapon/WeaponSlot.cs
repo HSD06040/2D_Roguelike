41f6ecf03be8a362c6e1aa35a46487e58a6ab950
woojinjang193 <ryanjang193@gmail.com>
2025-07-04 14:43:54 +0900

    [feat] UI 추가
    1. 플레이어 하트 이미지 적용
    2. 퀵슬롯 이미지 변경
    3. 악세서리, 일시정지 팝업창 변경
    4. 대사창 배경 변경

--- Assets/Scripts/UI/Weapon/WeaponSlot.cs
+++ Assets/Scripts/UI/Weapon/WeaponSlot.cs
@@ -5,13 +5,26 @@ using UnityEngine.UI;
 
 public class WeaponSlot : MonoBehaviour
 {
-    public Image[] weaponImages;
+    [SerializeField] private Image[] slots;   // 추가됨!!! 슬롯 배경 (회색/흰색)
+    [SerializeField] private Image[] weaponIcons;        // 추가됨!!! 아이템 아이콘
+
+    [SerializeField] private Sprite selectedSlotSprite;   // 추가됨!!! 선택됨 슬롯 배경 (흰색)
+    [SerializeField] private Sprite unselectedSlotSprite; // 추가됨!!! 선택 안됨 배경 (회색)
 
     private WeaponSlotPresenter presenter;
 
     private void Awake()
     {
         presenter = new WeaponSlotPresenter(this);
+    }
+
+    private void Start()
+    {
+        for(int i = 0; i < slots.Length; i++)
+        {
+            slots[i].sprite = unselectedSlotSprite;   //슬롯은 전부 비활성화 스프라이트로 시작
+            weaponIcons[i].gameObject.SetActive(false); //아이콘은 다 꺼줌
+        }
     }
 
     private void OnEnable()
@@ -26,20 +39,28 @@ public class WeaponSlot : MonoBehaviour
 
     public void ChangeSlot(int _idx)
     {
-        for (int i = 0; i < weaponImages.Length; i++)
+        for (int i = 0; i < slots.Length; i++)
         {
-            if(i == _idx)
-                weaponImages[i].color = Color.white;
+            if( i == _idx )
+            {
+                slots[i].sprite = selectedSlotSprite; //선택된건 흰색
+            }
             else
-                weaponImages[i].color = Color.clear;
+            {
+                slots[i].sprite = unselectedSlotSprite;
+            }
         }
     }
 
     public void UpdateWeaponSlot(int _idx, MusicWeapon _weapon)
     {
-        if (_weapon == null) return;
-        
-        weaponImages[_idx].sprite = _weapon.WeaponData.icon;
-        weaponImages[_idx].color = Manager.Data.PlayerStatus.currentWeaponIdx == _idx ? Color.white : Color.clear;
+        if (_weapon == null)
+        {
+            weaponIcons[_idx].gameObject.SetActive(false); // 아이템 없으면 아이콘 비활성화
+            return;
+        }
+
+        weaponIcons[_idx].gameObject.SetActive(true); // 아이템 있으면 아이콘 활성화
+        weaponIcons[_idx].sprite = _weapon.WeaponData.icon; // 아이콘 표시
     }
 }
