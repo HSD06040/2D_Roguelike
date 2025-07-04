6ad570eb6dce06a3c021e7be7a08421c8035f5db
HSD06040 <tinto0604@gmail.com>
2025-07-04 14:26:23 +0900

    [Feat] 씬 수정

--- Assets/Scripts/Interactable/Box/Box.cs
+++ Assets/Scripts/Interactable/Box/Box.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Xml.Serialization;
 using UnityEngine;
 using UnityEngine.AI;
-using static UnityEditor.Progress;
 
 public class Box : MonoBehaviour, IInteractable
 {
