d33e63f6784b6150c4b928474eebb6ef0ac2e441
HSD06040 <tinto0604@gmail.com>
2025-07-07 20:53:43 +0900

    [Fix] Tobeben 공격 패턴 수정
    1개의 패턴이 안나오던 문제를 수정하였다.

--- Assets/Scripts/Boss/Boss1/BossBaseState.cs
+++ Assets/Scripts/Boss/Boss1/BossBaseState.cs
@@ -1,5 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class BossBaseState<T> where T : BossMonsterFSM
@@ -20,16 +22,19 @@ public class BossBaseState<T> where T : BossMonsterFSM
     {
         fsm.Owner.Animator.SetBool(animHash, true);
         fsm.animFinish = false;
+        Debug.Log(this.GetType().Name + ": " + MethodBase.GetCurrentMethod().Name);
     }
 
     public virtual void Exit() 
     {
         fsm.Owner.Animator.SetBool(animHash, false);
+        Debug.Log(this.GetType().Name + ": " + MethodBase.GetCurrentMethod().Name);
     }
 
     public virtual void Update()
     {
         if(useTimer && timer >= 0)
-            timer -= Time.deltaTime;        
+            timer -= Time.deltaTime;
+        Debug.Log(this.GetType().Name + ": " + MethodBase.GetCurrentMethod().Name);
     }    
 }
