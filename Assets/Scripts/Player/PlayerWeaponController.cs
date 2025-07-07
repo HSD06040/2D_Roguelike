a1093240155fbb8ebc0a1ab44e4037f6ddd1f146
woojinjang193 <ryanjang193@gmail.com>
2025-07-07 14:41:40 +0900

    [edit] 씬 수정
    플레이어 프리팹에 빛 추가,
    모든 씬에서 빛 제거,
    몬스터 소환 안되는 방에서 몬스터 스포너 제거

--- Assets/Scripts/Player/PlayerWeaponController.cs
+++ Assets/Scripts/Player/PlayerWeaponController.cs
@@ -60,7 +60,7 @@ public class PlayerWeaponController : MonoBehaviour
         {
             if (delay < maxCount)
             {
-                Debug.Log("curWeapon");
+                //Debug.Log("curWeapon");
                 delay += Time.deltaTime;
                 canAttack = false;
             }
@@ -73,14 +73,14 @@ public class PlayerWeaponController : MonoBehaviour
         {
             if (delay < defaultMaxCount)
             {
-                Debug.Log("defaultWeapon");
-                Debug.Log($"defaultMaxCount : {defaultMaxCount}");
+                //Debug.Log("defaultWeapon");
+                //Debug.Log($"defaultMaxCount : {defaultMaxCount}");
                 delay += Time.deltaTime;
                 canAttack = false;
             }
             else
             {
-                Debug.Log("defaultWeapon can");
+                //Debug.Log("defaultWeapon can");
                 canAttack = true;
             }
         }
@@ -114,7 +114,7 @@ public class PlayerWeaponController : MonoBehaviour
 
     private void Attack(InputAction.CallbackContext ctx)
     {
-        Debug.Log($"canattack : {canAttack}");
+        //Debug.Log($"canattack : {canAttack}");
         if (!canAttack) return;
 
         if (currentWeapon != null)
